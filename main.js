e58f3bd9ac5b477d26ea4c67df7c83840e8c65d3
john beltrez <beltrezjohn1@gmail.com>
2024-10-29 10:26:40 -0500

    Part 2 and 3 completed

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ try {
 }
 
 console.log('...The execution continues'); 
+
+// Part 2 
+function flattenThis(arr) {
+    const a = [];
+    arr.forEach(e) {
+        if (Array.isArray(e) ) {
+            a.push(e.flatten)
+        }
+    }
+
+}
+
+// Part 3 
+
+const trampoline = (flattenThis, ...args) => {
+    let result = flattenThis(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  }
